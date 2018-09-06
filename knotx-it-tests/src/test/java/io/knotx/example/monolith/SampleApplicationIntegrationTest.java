6c988670f3176e35545af48f64245304812a9726
Mikołaj Wawrowski <mikolaj.wawrowski@cognifde.com>
2018-09-06 15:56:25 +0200

    Move tests to mocks, add test content from knotx-mocks

--- knotx-it-tests/src/test/java/io/knotx/example/monolith/SampleApplicationIntegrationTest.java
+++ knotx-it-tests/src/test/java/io/knotx/example/monolith/SampleApplicationIntegrationTest.java
@@ -15,25 +15,36 @@
  */
 package io.knotx.example.monolith;
 
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlMatching;
+import static io.knotx.junit5.util.RequestUtil.subscribeToResult_shouldSucceed;
+import static io.knotx.junit5.wiremock.KnotxWiremockExtension.stubForServer;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import com.github.tomakehurst.wiremock.WireMockServer;
 import io.knotx.dataobjects.AdapterResponse;
 import io.knotx.dataobjects.ClientResponse;
 import io.knotx.junit5.KnotxApplyConfiguration;
 import io.knotx.junit5.KnotxExtension;
 import io.knotx.junit5.util.FileReader;
+import io.knotx.junit5.wiremock.KnotxWiremock;
 import io.knotx.proxy.AdapterProxy;
 import io.netty.handler.codec.http.HttpResponseStatus;
+import io.reactivex.Single;
 import io.vertx.core.Future;
 import io.vertx.core.json.JsonObject;
 import io.vertx.junit5.VertxTestContext;
 import io.vertx.reactivex.core.MultiMap;
 import io.vertx.reactivex.core.Vertx;
+import io.vertx.reactivex.core.buffer.Buffer;
+import io.vertx.reactivex.ext.web.client.HttpResponse;
 import io.vertx.reactivex.ext.web.client.WebClient;
 import io.vertx.serviceproxy.ServiceBinder;
 import java.util.Map;
 import org.apache.commons.lang3.StringUtils;
 import org.jsoup.Jsoup;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
@@ -48,6 +59,34 @@ public class SampleApplicationIntegrationTest {
   private static final String LOCAL_MULTIPLE_FORMS_URI = "/content/local/multiple-forms.html";
   private static final int KNOTX_SERVER_PORT = 9092;
   private static final String KNOTX_SERVER_ADDRESS = "localhost";
+
+  @KnotxWiremock(port = 4001)
+  protected WireMockServer mockRepository;
+
+  @KnotxWiremock(port = 4002)
+  protected WireMockServer mockService;
+
+  @BeforeAll
+  void initMocks() {
+    stubForServer(mockRepository,
+        get(urlMatching("/content/.*"))
+            .willReturn(
+                aResponse()
+                    .withHeader("Cache-control", "no-cache, no-store, must-revalidate")
+                    .withHeader("Content-Type", "text/html; charset=UTF-8")
+                    .withHeader("X-Server", "Knot.x")
+            ));
+
+    //fixme add bouncer from original verticle
+    stubForServer(mockService,
+        get(urlMatching("/content/.*"))
+            .willReturn(
+                aResponse()
+                    .withHeader("Cache-control", "no-cache, no-store, must-revalidate")
+                    .withHeader("Content-Type", "text/html; charset=UTF-8")
+                    .withHeader("X-Server", "Knot.x")
+            ));
+  }
 
   @Test
   @KnotxApplyConfiguration("knotx-test-app.conf")
@@ -73,7 +112,7 @@ public class SampleApplicationIntegrationTest {
   }
 
   @Test
-  @KnotxApplyConfiguration({"knotx-test-app.conf"})
+  @KnotxApplyConfiguration("knotx-test-app.conf")
   public void whenRequestingLocalPageWhereInServiceIsMissingResponseBody_expectNoBodyHtml(
       VertxTestContext context, Vertx vertx) {
     testGetRequest(context, vertx, LOCAL_NO_BODY_REQUEST_URI, "noBody.html");
@@ -133,14 +172,15 @@ public class SampleApplicationIntegrationTest {
       String expectedResponseFile) {
 
     WebClient client = WebClient.create(vertx);
-    client.post(KNOTX_SERVER_PORT, KNOTX_SERVER_ADDRESS, url)
-        .rxSendForm(getMultiMap(formData))
-        .doOnSuccess(resp -> {
-          assertEquals(resp.statusCode(), HttpResponseStatus.OK.code());
-          assertEquals(Jsoup.parse(resp.body().toString()).body().html(),
-              Jsoup.parse(FileReader.readTextSafe(expectedResponseFile)).body().html());
-        })
-        .subscribe(onSuccess -> context.completeNow(), context::failNow);
+    Single<HttpResponse<Buffer>> httpResponseSingle = client
+        .post(KNOTX_SERVER_PORT, KNOTX_SERVER_ADDRESS, url)
+        .rxSendForm(getMultiMap(formData));
+
+    subscribeToResult_shouldSucceed(context, httpResponseSingle, resp -> {
+      assertEquals(Jsoup.parse(FileReader.readTextSafe(expectedResponseFile)).body().html(),
+          Jsoup.parse(resp.body().toString()).body().html());
+      assertEquals(HttpResponseStatus.OK.code(), resp.statusCode());
+    });
   }
 
   private MultiMap getMultiMap(Map<String, Object> formData) {
@@ -152,24 +192,27 @@ public class SampleApplicationIntegrationTest {
   private void testGetRequest(VertxTestContext context, Vertx vertx, String url,
       String expectedResponseFile) {
     WebClient client = WebClient.create(vertx);
-    client.get(KNOTX_SERVER_PORT, KNOTX_SERVER_ADDRESS, url).rxSend()
-        .doOnSuccess(resp -> {
-          assertEquals(resp.statusCode(), HttpResponseStatus.OK.code());
-          assertEquals(Jsoup.parse(resp.body().toString()).body().html().trim(),
-              Jsoup.parse(FileReader.readTextSafe(expectedResponseFile)).body().html().trim());
-          client.close();
-        })
-        .subscribe(onSuccess -> context.completeNow(), context::failNow);
+    Single<HttpResponse<Buffer>> httpResponseSingle = client
+        .get(KNOTX_SERVER_PORT, KNOTX_SERVER_ADDRESS, url).rxSend();
+
+    subscribeToResult_shouldSucceed(context, httpResponseSingle, resp -> {
+      assertEquals(Jsoup.parse(FileReader.readTextSafe(expectedResponseFile)).body().html().trim(),
+          Jsoup.parse(resp.body().toString()).body().html().trim());
+      assertEquals(HttpResponseStatus.OK.code(), resp.statusCode());
+      client.close();
+    });
   }
 
   private void testGetServerError(VertxTestContext context, Vertx vertx, String url) {
     WebClient client = WebClient.create(vertx);
-    client.get(KNOTX_SERVER_PORT, KNOTX_SERVER_ADDRESS, url).rxSend()
-        .doOnSuccess(resp -> {
-          assertEquals(resp.statusCode(), HttpResponseStatus.INTERNAL_SERVER_ERROR.code());
-          client.close();
-        })
-        .subscribe(onSuccess -> context.completeNow(), context::failNow);
+    Single<HttpResponse<Buffer>> httpResponseSingle = client
+        .get(KNOTX_SERVER_PORT, KNOTX_SERVER_ADDRESS, url)
+        .rxSend();
+
+    subscribeToResult_shouldSucceed(context, httpResponseSingle, resp -> {
+      assertEquals(HttpResponseStatus.INTERNAL_SERVER_ERROR.code(), resp.statusCode());
+      client.close();
+    });
   }
 
   private JsonObject getFirstTestFormData() {
