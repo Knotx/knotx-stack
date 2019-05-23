0efcd7932b59be1d37421bcc69c78586d141664d
tomasz.michalak <tomek.michalak@gmail.com>
2019-05-23 10:25:13 +0200

    Configure rat plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,33 @@
+/*
+ * Copyright (C) 2019 Knot.x Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import org.nosphere.apache.rat.RatTask
+
 plugins {
     id("java-library")
     id("maven-publish")
     id("signing")
+    id("org.nosphere.apache.rat") version "0.4.0"
 }
 
 project.group = "io.knotx"
 
+// we do not use mavenLocal - instead please setup composite build environment (https://github.com/Knotx/knotx-aggregator)
 repositories {
     jcenter()
+    maven { url = uri("https://plugins.gradle.org/m2/") }
     maven { url = uri("https://oss.sonatype.org/content/groups/staging/") }
     maven { url = uri("https://oss.sonatype.org/content/repositories/snapshots") }
 }
@@ -15,6 +35,16 @@ repositories {
 apply(from = "gradle/javaAndUnitTests.gradle.kts")
 apply(from = "gradle/integrationTests.gradle.kts")
 apply(from = "gradle/distribution.gradle.kts")
+
+// -----------------------------------------------------------------------------
+// License headers validation
+// -----------------------------------------------------------------------------
+tasks {
+    named<RatTask>("rat") {
+        excludes.addAll("*.md", "**/*.md", "**/bin/*", ".travis.yml", "**/build/*", "**/out/*", "**/*.json", "**/*.conf", "**/*.html", "**/*.properties", ".idea")
+    }
+    getByName("build").dependsOn("rat")
+}
 
 // -----------------------------------------------------------------------------
 // Publication
