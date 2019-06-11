5aea9e369ca26a1077dedeb1e3d470d3f46af2cf
Piotr Marcinkowski <piotr.marcinkowski@cognifide.com>
2019-06-11 13:40:54 +0200

    Not signed local maven publication

--- build.gradle.kts
+++ build.gradle.kts
@@ -114,6 +114,12 @@ publishing {
         }
     }
 }
+val subProjectPath = this.path
 signing {
+    setRequired({
+        gradle.taskGraph.hasTask("$subProjectPath:publish") ||
+                gradle.taskGraph.hasTask("$subProjectPath:publishMavenJavaPublicationToMavenRepository")
+    })
+
     sign(publishing.publications["knotxDistribution"])
 }
