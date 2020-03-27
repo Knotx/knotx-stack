d22624ba615556640701fa82755febac863534b2
Oskar Jerzyk <oskar.jerzyk@cognifide.com>
2020-03-27 10:24:14 +0100

    test configuration created

---
+++ src/functionalTest/java/io/knotx/stack/functional/KnotxConsumersIntegrationTest.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (C) 2019 Knot.x Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.knotx.stack.functional;
+
+import com.github.tomakehurst.wiremock.WireMockServer;
+import io.knotx.junit5.KnotxApplyConfiguration;
+import io.knotx.junit5.KnotxExtension;
+import io.knotx.junit5.RandomPort;
+import io.knotx.junit5.wiremock.ClasspathResourcesMockServer;
+import io.knotx.stack.KnotxServerTester;
+import io.vertx.junit5.VertxTestContext;
+import io.vertx.reactivex.core.Vertx;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+@ExtendWith(KnotxExtension.class)
+@TestInstance(Lifecycle.PER_CLASS)
+public class KnotxConsumersIntegrationTest {
+
+  @ClasspathResourcesMockServer
+  private WireMockServer mockService;
+
+  @Test
+  @DisplayName("Should return fragment properly processed by consumer")
+  @KnotxApplyConfiguration({"conf/application.conf",
+      "scenarios/knotx-consumers-integration/mocks.conf",
+      "scenarios/knotx-consumers-integration/tasks.conf"})
+  void processFragment(VertxTestContext testContext, Vertx vertx,
+      @RandomPort Integer globalServerPort) {
+    KnotxServerTester serverTester = KnotxServerTester.defaultInstance(globalServerPort);
+    serverTester.testGet(testContext, vertx, "/api/consumer", response -> {
+      response.hashCode();
+    });
+  }
+}
